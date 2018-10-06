a927228af0b48de712325ad00c1d0e42c2671904
Rahul7star <rahul7star@gmail.com>
2018-10-06 10:35:52 +1000

    Json Rendering
    React Native Json Parser showing Image and Text

--- my-app/App.js
+++ my-app/App.js
@@ -1,78 +1,144 @@
+
+  
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
-import { Text, View } from 'react-native';
-
-import {
-  createStackNavigator,
-} from 'react-navigation';
-
-
-
-
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    /*setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);*/
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-
-
-
-export default class Bananas extends Component {
-
-
-
- static navigationOptions = {
-    title: 'Welcome',
-  };
-  render() {
-//const { navigate } = this.props.navigation;
-
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-
-      <View>
-      <Image source={pic} style={{width: 193, height: 110}}/>
-
+ 
+import { StyleSheet, Platform, View, ActivityIndicator, FlatList, Text, Image, Alert, YellowBox } from 'react-native';
+ 
+export default class Project extends Component {
+ 
+ constructor(props) {
+ 
+   super(props);
+ 
+   this.state = {
+ 
+     isLoading: true
+ 
+   }
+ 
+   YellowBox.ignoreWarnings([
+    'Warning: componentWillMount is deprecated',
+    'Warning: componentWillReceiveProps is deprecated',
+  ]);
+ 
+ }
+ 
+GetItem (title) {
+  
+ Alert.alert(title);
+ 
+ }
+ 
+ FlatListItemSeparator = () => {
+   return (
+     <View
+       style={{
+         height: .5,
+         width: "100%",
+         backgroundColor: "#000",
+       }}
+     />
+   );
+ }
+ 
+ webCall=()=>{
+ 
+  return fetch('http://innokria.com/LOA/Json/saru_list')
+         .then((response) => response.json())
+         .then((responseJson) => {
+           this.setState({
+             isLoading: false,
+            dataSource: responseJson.data,
+           }, function() {
+             // In this block you can do something with new state.
+           });
+         })
+         .catch((error) => {
+           console.error(error);
+         });
+ 
+ }
+ 
+ componentDidMount(){
+ 
+  this.webCall();
+ 
+ }
+ 
+ render() {
+ 
+   if (this.state.isLoading) {
+     return (
+ 
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+ 
+         <ActivityIndicator size="large" />
+ 
+       </View>
        
-        <Text>Login Page UI Will be added soon!</Text>
-
-
-         <Blink text='I am testing UI' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-
-
-      </View>
-
-    );
-
-    /*return (
-      <View>
-        <Text>Hello world!</Text>
-      </View>
-    );*/
-  }
-  
-  
+     );
+ 
+   }
+ 
+   return (
+ 
+     <View style={styles.MainContainer}>
+ 
+       <FlatList
+       
+        data={ this.state.dataSource }
+        
+        ItemSeparatorComponent = {this.FlatListItemSeparator}
+ 
+        renderItem={({item}) => 
+        
+            <View style={{flex:1, flexDirection: 'row'}}>
+    
+              <Image source = {{ uri: item.link }} style={styles.imageView} />
+            
+              <Text onPress={this.GetItem.bind(this, item.title)} 
+              style= {styles.textView} >{item.title}</Text>
+ 
+            </View>
+        
+          }
+ 
+        keyExtractor={(item, index) => index.toString()}
+        
+        />
+ 
+     </View>
+   );
+ }
 }
-
+ 
+const styles = StyleSheet.create({
+ 
+MainContainer :{
+ 
+    justifyContent: 'center',
+    flex:1,
+    margin: 5,
+    marginTop: (Platform.OS === 'ios') ? 20 : 0,
+ 
+},
+ 
+imageView: {
+ 
+    width: '50%',
+    height: 100 ,
+    margin: 7,
+    borderRadius : 7
+ 
+},
+ 
+textView: {
+ 
+    width:'50%', 
+    textAlignVertical:'center',
+    padding:10,
+    color: '#000'
+ 
+}
+ 
+});
