41953bcf5df3e3039d99d7928687bbb7db0e992a
Rahul7star <rahul7star@gmail.com>
2018-10-13 09:59:48 +1100

    Google Map Added
    Google Map API added,
    REST API JSON updated

--- my-app/Friend.js
+++ my-app/Friend.js
@@ -1,7 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
-
+import { StyleSheet, Text, View ,Image,Button,YellowBox } from 'react-native';
+import getDirections from 'react-native-google-maps-directions'
 export default class Friend extends React.Component {
+
+   constructor(props) {
+ 
+   super(props);
+ 
+   this.state = {
+ 
+     isLoading: true
+ 
+   }
+
+ 
+ 
+   YellowBox.ignoreWarnings([
+    'Warning: componentWillMount is deprecated',
+    'Warning: componentWillReceiveProps is deprecated',
+  ]);
+ 
+ }
+
+
+handleGetDirections = (lat,lon) => {
+
+  
+
+   
+    const data = {
+       source: {
+        latitude:  "",
+        longitude:  ''
+      },
+      destination: {
+        latitude:  lat,
+        longitude: lon
+      },
+      params: [
+        {
+          key: "travelmode",
+          value: "driving"        // may be "walking", "bicycling" or "transit" as well
+        },
+        {
+          key: "dir_action",
+          value: "navigate"       // this instantly initializes navigation using the given travel mode 
+        }
+      ]
+    }
+
+    getDirections(data)
+  }
 
 
   render() {
@@ -27,8 +76,7 @@ export default class Friend extends React.Component {
               {this.props.navigation.state.params.item.description}
            </Text>
 
-
-           
+          <Button onPress={() => this.handleGetDirections(this.props.navigation.state.params.item.latitude,this.props.navigation.state.params.item.longitude)}  title="Get Directions" />
 
       </View>
     );
